9c6e243cb8a146e95298995394b9fd818bec5289
Shivraj Roy <shivrajroy78@gmail.com>
2025-04-12 00:42:51 +0530

    🎬  initialize Neuronix || saas webapp...

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Header from "./section/Header";
+
+function App() {
+   return (
+      <main className="overflow-hidden">
+         <Header />
+      </main>
+   );
+}
+export default App;
---
+++ src/section/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+   return (
+      <>
+         <div>Neuronix</div>
+      </>
+   );
+};
+export default Header;
