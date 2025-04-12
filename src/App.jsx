d69c5482f6d63b835d9f56f64138fe2316225681
Shivraj Roy <shivrajroy78@gmail.com>
2025-04-12 21:32:28 +0530

    ⇩ add Download component for app download section...

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Features from "./components/Features";
 import Pricing from "./components/Pricing";
 import Faq from "./components/Faq";
 import Testimonials from "./components/Testimonials";
+import Download from "./components/Download";
 
 function App() {
    return (
@@ -14,6 +15,7 @@ function App() {
          <Pricing />
          <Faq />
          <Testimonials />
+         <Download />
       </main>
    );
 }
